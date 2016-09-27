c3466c5bbd86e6a3ebf498a8561688e6c0c073c3
Keo  Bun <keonilrath@gmail.com>
2016-09-27 04:58:55 +0000

    scraper started

---
+++ lib/scraper.rb
@@ -0,0 +1,10 @@
+require 'open'
+require 'pry'
+
+class Scraper
+
+  def self.scrape_the_counted
+    Nokogiri::HTML(open("https://www.theguardian.com/us-news/ng-interactive/2015/jun/01/the-counted-police-killings-us-database#"))
+    binding.pry
+  end
+end
